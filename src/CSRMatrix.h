05c8b2cd9dfcaaa2f3eb599e2e1fe5f2dfdfcf08
周毅 <zhouyi.um@gmail.com>
2020-10-16 15:08:20 +0800

    Add row vector operators.

--- src/CSRMatrix.h
+++ src/CSRMatrix.h
@@ -14,7 +14,11 @@ class SparseMatrix;
 template <typename elementType>
 class CSRMatrix : SparseMatrix{
 public:
-    CSRMatrix(unsigned long long rows, unsigned long long cols) : SparseMatrix(rows, cols) {}
+    //! default constructor
+    CSRMatrix(unsigned long long rows, unsigned long long cols);
+
+    //! default constructor
+    CSRMatrix(const CSRMatrix& m);
 
     ~CSRMatrix() {}
 
@@ -24,6 +28,12 @@ public:
         //TBD
     }
 
+public:
+    // accessors and mutators for class members
+    std::vector<elementType>&        rowVector(void);
+    const std::vector<elementType>&  rowVector(void) const;
+    void                             rowVector(const std::vector<elementType>& v);
+
     /* support operators */
 public:
     //matrix operator methods
@@ -31,35 +41,8 @@ public:
     //SparseMatrix    operator+(const SparseMatrix& m) const;
 
 public:
-    void ToMatrix() {
-        unsigned long long rows = getRows();
-        unsigned long long cols = getCols();
-        unsigned long long i, j;
-        elementType **array;//指向指针的指针，表示一个二维数组
-        array=new elementType *[rows];//申请行
-        for (i=0; i<rows; i++) {
-            array[i]=new elementType[cols];//申请列
-        }
-        std::cout<<"row nums is "<<rows<<" col nums is "<<cols<<"\n";
-
-        //这里使用随机数
-        int maxx=200;
-        int minx=1;
-
-        srand((unsigned int)time(NULL));
-        for (i=0; i<rows; i++) {
-            for (j=0; j<cols; j++) {
-                int x=rand()%(maxx-minx+1)+minx;
-                std::cout<<x<<' ';
-            }
-            std::cout<<'\n';
-        }
-
-        for (i=0; i<rows; i++) {
-            delete[] array[i];//释放列
-        }
-        delete[] array;//释放行        
-    }
+    //! print the matrix
+    void ToMatrix();
 
 private:
     //! row index pointer vector
@@ -71,6 +54,60 @@ private:
     //! vector of non-zero elements and diagonal elements of matrix
     std::vector<elementType> _data;
 }; //end of class CSRMatrix
+
+//! default constructor
+template <typename elementType>
+CSRMatrix<elementType>::CSRMatrix(unsigned long long rows, unsigned long long cols) : SparseMatrix(rows, cols) {
+}
+
+//! return row pointer vector
+template <typename elementType>
+inline std::vector<elementType>& CSRMatrix<elementType>::rowVector(void) { 
+    return this->_row; 
+}
+
+//! return copy of row pointer vector
+template <typename elementType>
+inline const std::vector<elementType>& CSRMatrix<elementType>::rowVector(void) const { 
+    return _row; 
+}
+
+//! set row pointer vector to specified vector 
+template <typename elementType>
+inline void CSRMatrix<elementType>::rowVector(const std::vector<elementType>& v) { 
+    _row = v; 
+}
+
+template <typename elementType>
+void CSRMatrix<elementType>::ToMatrix() {
+    unsigned long long rows = getRows();
+    unsigned long long cols = getCols();
+    unsigned long long i, j;
+    elementType **array;//指向指针的指针，表示一个二维数组
+    array=new elementType *[rows];//申请行
+    for (i=0; i<rows; i++) {
+        array[i]=new elementType[cols];//申请列
+    }
+    std::cout<<"row nums is "<<rows<<" col nums is "<<cols<<"\n";
+
+    //这里使用随机数
+    int maxx=200;
+    int minx=1;
+
+    srand((unsigned int)time(NULL));
+    for (i=0; i<rows; i++) {
+        for (j=0; j<cols; j++) {
+            int x=rand()%(maxx-minx+1)+minx;
+            std::cout<<x<<' ';
+        }
+        std::cout<<'\n';
+    }
+
+    for (i=0; i<rows; i++) {
+        delete[] array[i];//释放列
+    }
+    delete[] array;//释放行        
+}
 
 //} //end of namespace
 
